63819c6e01df6dc233e59f916fb2c591be9f7c12
Eric Rabil <EricRabil@users.noreply.github.com>
2017-06-29 14:10:00 -0400

    Nickname command

--- src/commands/nickname.js
+++ src/commands/nickname.js
@@ -1,17 +1,34 @@
 const CastAPI = require(`castapi`);
 const Command = CastAPI.Command;
+const Discord = require(`discord.js`);
 
 class Nickname extends Command {
-  clear(message, response, args) {
-    var target = message.mentions.members.size == 0 ? message.member : message.mentions.members.first();
-    target.setNickname('').then(() => {
-      return response.reply(`Successfully cleared the nickname for <@${target.id}>`)
+  update(message, response, nickname, clearing = false) {
+    var targets = message.mentions.members.size == 0 ? [message.member] : message.mentions.members.array();
+    var success = {active: false, text: `Successfully ${clearing ? `cleared` : `updated`} the nickname for`};
+    var failed = {active: false, text: `Couldn't ${clearing ? `clear` : `update`} the nickname for `}
+    var count = 0;
+    var reply = () => response.reply(`${success.active ? success.text : ``}${failed.active ? `${success.active ? `; ` : ``}${failed.text}` : ``}`);
+    targets.forEach((target, index) => {
+      target.setNickname(nickname).then(() => {
+        count++;
+        success.active = true;
+        success.text += ` <@${target.id}> `
+        if (count === targets.length) return reply();
+      }).catch(e => {
+        count++;
+        failed.active = true;
+        failed.text += `<@${target.id}> `
+        if (count === targets.length) return reply();
+      })
     })
+    
   }
   
   execute(message, response, args) {
-    if ((args.length == 0) || (message.mentions.members.size == args.length)) return this.clear(message, response, args);
-    
+    if ((args.length == 0) || (message.mentions.members.size == args.length)) return this.update(message, response, '', true);
+    var nick = args.join(" ").replace(Discord.MessageMentions.USERS_PATTERN, '').trim();
+    this.update(message, response, nick);
   }
 }
 
