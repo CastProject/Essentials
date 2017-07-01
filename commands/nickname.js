8b98c98590ccb4c013d9a29e0d3f56d5d7c7db1e
Eric Rabil <EricRabil@users.noreply.github.com>
2017-07-01 11:48:05 -0400

    Added error explanation

--- commands/nickname.js
+++ commands/nickname.js
@@ -6,7 +6,7 @@ class Nickname extends Command {
   update(message, response, nickname, clearing = false) {
     var targets = message.mentions.members.size == 0 ? [message.member] : message.mentions.members.array();
     var success = {active: false, text: `Successfully ${clearing ? `cleared` : `updated`} the nickname for`};
-    var failed = {active: false, text: `Couldn't ${clearing ? `clear` : `update`} the nickname for `}
+    var failed = {active: false, text: `Couldn't ${clearing ? `clear` : `update`} nickname(s) for `}
     var count = 0;
     var reply = () => response.reply(`${success.active ? success.text : ``}${failed.active ? `${success.active ? `; ` : ``}${failed.text}` : ``}`);
     targets.forEach((target, index) => {
@@ -19,7 +19,10 @@ class Nickname extends Command {
         count++;
         failed.active = true;
         failed.text += `<@${target.id}> `
-        if (count === targets.length) return reply();
+        if (count === targets.length) {
+          failed.text += ` because ${e.message.toLowerCase()}`
+          return reply();
+        }
       })
     })
     
